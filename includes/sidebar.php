0622244ae58168f68dc12876f7465d1452330a55
senn-komi <2411500046@mahasiswa.atmaluhur.ac.id>
2025-11-13 22:52:39 +0700

    add: sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -66,7 +66,7 @@
                         <a class="nav-link" href="layout-static.html">Konfirmasi</a>
                     </nav>
                 </div>
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="logout.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-sign-out"></i></div>
                     Logout
                 </a>
@@ -74,7 +74,7 @@
         </div>
         <div class="sb-sidenav-footer">
             <div class="small">Logged in as:</div>
-            Start Bootstrap
+            <?php echo $_SESSION['admin_nama_lengkap'] ?>
         </div>
     </nav>
 </div>
